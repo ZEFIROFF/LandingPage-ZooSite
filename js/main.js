5c488d7b931fceb2d6658d3cfe456f72faae9c4d
Денис Зефиров <55986656+d3pii@users.noreply.github.com>
2020-03-09 21:34:24 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ $('.count').each(function () {
         }
     });
 });
+
+$(document).ready(function (jQuery) {
+    $(window).scroll(function () {
+        let sticky = $('.header_block_menu'),
+            scroll = $(window).scrollTop();
+
+        if (scroll >= 200) sticky.addClass('fixed');
+        else sticky.removeClass('fixed');
+    });
+});
+
+$(document).ready(function () {
+    $('.fa-bars').click(function (event) {
+        $('fa-bars, .menu_up').toggleClass('active_menu');
+    });
+});
